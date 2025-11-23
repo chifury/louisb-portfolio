502435a5300ca41b9fc529865dafa3d0c306240d
Louis Bourne <chifury@proton.me>
2025-11-24 00:45:49 +0700

    Style: styled the edit account page and created the profile page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,12 @@ class PagesController < ApplicationController
   def home
     @projects = Project.all
   end
+
+  def profile
+    @user = current_user
+    @projects = Project.all
+    @projects_total = Project.all.count
+    @personal_projects_count = Project.where(personal_project: true).count
+    @non_personal_projects_count = Project.where(personal_project: false).count
+  end
 end
