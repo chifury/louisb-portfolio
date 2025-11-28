1f6620905f43db14973604f1b516560e39953c07
Louis Bourne <chifury@proton.me>
2025-11-29 03:00:11 +0700

    Feat: made nav-items in the navbar highlight active if it is the current page for logged in user.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def nav_link_class(path = "#")
+    current_page?(path) ? "nav-link active" : "nav-link"
+  end
+
+  def nav_link_dropdown_class(path = "#")
+    current_page?(path) ? "dropdown-item active" : "dropdown-item"
+  end
 end
